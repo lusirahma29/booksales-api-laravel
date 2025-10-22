57ff94c3a195ab37ad0f74793bc8b0715ef46d0a
lusirahma29 <lusirahmajobs@gmail.com>
2025-10-22 23:04:33 +0700

    updated file, laravel pertemuan 7

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,7 +17,8 @@ class DatabaseSeeder extends Seeder
             GenreSeeder::class,
             AuthorSeeder::class,
             BookSeeder::class,
-            UserSeeder::class
+            UserSeeder::class,
+            TransactionSeeder::class
        ]);
     }
 }
